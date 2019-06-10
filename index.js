48e3528f5d5642b0730eee37bffa108528114196
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-10 13:39:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyPrependKitten(name){
 }
 
 function destructivelyRemoveLastKitten(){
-  // kittens.slice(kittens.lenght -1)
+  kittens= kittens.slice(kittens.lenght -1)
   return kittens
 }
